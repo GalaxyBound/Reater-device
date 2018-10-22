9c52825654116f3cc429b72f7a89bab8bd60a18c
Josh <josh@infinx.com.au>
2018-10-22 16:51:19 +1100

    Added HTTP communications

--- reater.m
+++ reater.m
@@ -1,3 +1,6 @@
+import matlab.net.*
+import matlab.net.http.*
+
 if ~isempty(instrfind)
      fclose(instrfind);
       delete(instrfind);
@@ -17,6 +20,17 @@ while (a==1)
     disp(raw)
     
     % TODO: process the raw data here
+    % [response,completedrequest,history] = send(request,uri)
+    r = RequestMessage;
+    uri = URI('http://127.0.0.1:8000/ehealth/test');
+    resp = send(r,uri);
+    status = resp.StatusCode
+    bodyData = resp.Body.Data
+    
+    % you can split the bodyData up into whatever data you need
+    
+    % Delay for 30s cause we dont want to spam server right now
+    pause(30);
 end
 
 fclose(comport);
