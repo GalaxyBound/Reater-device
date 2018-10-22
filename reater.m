b8aecfb089f92d3768b7cc2c6a85fdaaeec5f2c3
Josh <josh@infinx.com.au>
2018-10-22 17:47:04 +1100

    Added easy POST requests

--- reater.m
+++ reater.m
@@ -19,18 +19,31 @@ while (a==1)
     raw = fscanf(comport);
     disp(raw)
     
-    % TODO: process the raw data here
-    % [response,completedrequest,history] = send(request,uri)
-    r = RequestMessage;
+    % TODO: process the raw data here, then load into data (to send)
+    data = raw;
+    
+    % General headers
+%     body = matlab.net.http.MessageBody(data);
+%     contentTypeField = matlab.net.http.field.ContentTypeField('text/plain');
+%     type1 = matlab.net.http.MediaType('text/*');
+%     type2 = matlab.net.http.MediaType('application/json','q','.5');
+%     acceptField = matlab.net.http.field.AcceptField([type1 type2]);
+%     header = [acceptField contentTypeField];
+%     method = matlab.net.http.RequestMethod.POST;
+%     
+%     r = matlab.net.http.RequestMessage(method,header,body);
+
     uri = URI('http://127.0.0.1:8000/ehealth/test');
-    resp = send(r,uri);
-    status = resp.StatusCode
-    bodyData = resp.Body.Data
+    resp = webwrite(uri, 'raw-data', raw, 'test-name1', 'test-value1')
+%     resp = send(r,uri);
+%     status = resp.StatusCode
+    % bodyData = resp.Body.Data
     
     % you can split the bodyData up into whatever data you need
+    postData = resp.post
     
     % Delay for 30s cause we dont want to spam server right now
-    pause(30);
+    pause(10);
 end
 
 fclose(comport);
