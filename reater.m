b508b8591d2c863ae14efeeb8c55aa3bb73461f7
suyi3325 <42730276+suyi3325@users.noreply.github.com>
2018-10-29 13:16:51 +1100

    Add files via upload

--- reater.m
+++ reater.m
@@ -8,7 +8,7 @@ end
 
 % NEED TO MANUALLY SET BY USING ls /dev/tty.usbmodem* in terminal and replace below
 % string
-com_port = "/dev/tty.usbmodem1411";%com_list{end};
+com_port = "/dev/tty.usbmodem14311";%com_list{end};
 fprintf('Opening %s\n', com_port);
 comport = serial(com_port, 'BaudRate', 115200);
 fopen(comport);
@@ -19,28 +19,18 @@ while (a==1)
     raw = fscanf(comport);
     disp(raw)
     
-    % TODO: process the raw data here, then load into data (to send)
     data = raw;
-    
-    % General headers
-%     body = matlab.net.http.MessageBody(data);
-%     contentTypeField = matlab.net.http.field.ContentTypeField('text/plain');
-%     type1 = matlab.net.http.MediaType('text/*');
-%     type2 = matlab.net.http.MediaType('application/json','q','.5');
-%     acceptField = matlab.net.http.field.AcceptField([type1 type2]);
-%     header = [acceptField contentTypeField];
-%     method = matlab.net.http.RequestMethod.POST;
-%     
-%     r = matlab.net.http.RequestMessage(method,header,body);
-
+    % TODO: process the raw data here
+    % [response,completedrequest,history] = send(request,uri)
+    % r = RequestMessage;
     uri = URI('http://127.0.0.1:8000/ehealth/test');
     resp = webwrite(uri, 'raw-data', raw, 'test-name1', 'test-value1')
-%     resp = send(r,uri);
-%     status = resp.StatusCode
+    % resp = send(r,uri);
+    % status = resp.StatusCode
     % bodyData = resp.Body.Data
     
-    % you can split the bodyData up into whatever data you need
     postData = resp.post
+    % you can split the bodyData up into whatever data you need
     
     % Delay for 30s cause we dont want to spam server right now
     pause(10);
